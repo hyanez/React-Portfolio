b63f7fbef706c83a008ad71495ffbe72a33ad6b5
Hugo Yanez <hugodavidyanez@gmail.com>
2022-01-04 08:54:41 -0500

    Added styling to each project card box

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,43 +1,58 @@
 import React from "react";
 import "../styles/Project.css";
+import ftnsJnks from "../images/ftns-jnks-demo.png";
+import weatherDash from "../images/weather-dashboard-mockup-2.png";
+import foodE from "../images/FoodE-proj.png";
 
 export default function Project() {
   return (
     <div class="project-header">
-      <h2>PROJECT HERE</h2>
+      <div class="proj-title">PROJECTS</div>
       <div class="project-section">
         <div class="first-proj">
+          <img src={foodE} alt="Ftns-Jnks Project" class="proj-photo"></img>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 class="card-title">FoodE</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              HTML5 / CSS / JS / Spoontacular API / Bulma
+            </h6>
             <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <div class="links">
-              <a href="#" class="card-link">
-                Card link
+              <a
+                href="https://mwathomas.github.io/project-1/"
+                class="card-link"
+              >
+                Deployed link
               </a>
-              <a href="#" class="card-link">
-                Another link
+              <a
+                href="https://github.com/mwathomas/project-1"
+                class="card-link"
+              >
+                Github link
               </a>
             </div>
           </div>
         </div>
         <div class="second-proj">
+          <img src={ftnsJnks} alt="Ftns-Jnks Project" class="proj-photo"></img>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 class="card-title">Ftns-Jnks</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              HTML5 / CSS / Node.JS / MySQL / Sequelize / Handlebars
+            </h6>
             <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <div class="links">
               <a href="#" class="card-link">
-                Card link
+                Deployed link
               </a>
-              <a href="#" class="card-link">
-                Another link
+              <a href="https://github.com/hyanez/Ftnss-Jnks" class="card-link">
+                Github link
               </a>
             </div>
           </div>
