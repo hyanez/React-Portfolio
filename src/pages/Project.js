d13e6250e160beb8250ea22ed8593dfdac45bd0a
Hugo Yanez <hugodavidyanez@gmail.com>
2022-07-16 21:48:21 -0400

    Added sixth project card after save

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -154,6 +154,24 @@ export default function Project() {
             </div>
           </div>
         </div>
+        <div class="sixth-proj">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div class="links">
+              <a href="#" class="card-link">
+                Card link
+              </a>
+              <a href="#" class="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
